59502caba5cf5c1412bc875e1a89e9708e0651ec
Philip Rodrigues <rodriges@fnal.gov>
2021-02-10 11:43:45 +0000

    Tell MsgPack not to copy binary fields on deserialization. Speeds up Fragment deserialization

--- include/serialization/Serialization.hpp
+++ include/serialization/Serialization.hpp
@@ -163,7 +163,20 @@ deserialize(const std::vector<CharType>& v)
       return j.get<T>();
     }
     case serialization_type_byte(kMsgPack): {
-      msgpack::object_handle oh = msgpack::unpack((char*)(v.data() + 1), v.size() - 1); // NOLINT
+      // The lambda function here is of type `unpack_reference_func`
+      // as described at
+      // https://github.com/msgpack/msgpack-c/wiki/v2_0_cpp_unpacker#memory-management
+      // . It is called for every STR, BIN and EXT field in the
+      // MsgPack data. If the function returns false, the object is
+      // copied into MsgPack's "zone", otherwise a pointer to the
+      // original buffer is stored. Our input buffer is going to exist
+      // at least until the end of this function, so it's safe to
+      // return true (ie, store a pointer in the MsgPack object; no
+      // copy) everywhere. Doing so results in a factor ~2 speedup in
+      // deserializing Fragment, which is just a large BIN field
+      msgpack::object_handle oh = msgpack::unpack((char*)(v.data() + 1),
+                                                  v.size() - 1,
+                                                  [](msgpack::type::object_type /*typ*/, std::size_t /*length*/, void* /*user_data*/) -> bool {return true;}); // NOLINT
       msgpack::object obj = oh.get();
       return obj.as<T>();
     }
