9576edb5254492fb0933fe893309f67a417d058e
Eric Flumerfelt <eflumerf@fnal.gov>
2022-04-08 11:05:31 -0500

    Clang-format

--- include/serialization/Serialization.hpp
+++ include/serialization/Serialization.hpp
@@ -20,8 +20,12 @@
 #include <string>
 #include <vector>
 
-#define DUNE_DAQ_SERIALIZABLE(Type) \
- template<> struct dunedaq::serialization::is_serializable<Type> { static constexpr bool value = true; }
+#define DUNE_DAQ_SERIALIZABLE(Type)                                                                                    \
+  template<>                                                                                                           \
+  struct dunedaq::serialization::is_serializable<Type>                                                                 \
+  {                                                                                                                    \
+    static constexpr bool value = true;                                                                                \
+  }
 
 /**
  * @brief Macro to make a class/struct serializable intrusively
@@ -135,7 +139,9 @@ ERS_DECLARE_ISSUE(serialization,                        // namespace
 
 namespace serialization {
 
-    template<typename T> struct is_serializable : std::false_type {};
+template<typename T>
+struct is_serializable : std::false_type
+{};
 
 /**
  * @brief Serialization methods that are available
@@ -239,10 +245,12 @@ deserialize(const std::vector<CharType>& v)
         // return true (ie, store a pointer in the MsgPack object; no
         // copy) everywhere. Doing so results in a factor ~2 speedup in
         // deserializing Fragment, which is just a large BIN field
-        msgpack::object_handle oh = msgpack::unpack(
-          const_cast<char*>(reinterpret_cast<const char*>(v.data() + 1)),
-          v.size() - 1,
-          [](msgpack::type::object_type /*typ*/, std::size_t /*length*/, void* /*user_data*/) -> bool { return true; });
+        msgpack::object_handle oh =
+          msgpack::unpack(const_cast<char*>(reinterpret_cast<const char*>(v.data() + 1)),
+                          v.size() - 1,
+                          [](msgpack::type::object_type /*typ*/, std::size_t /*length*/, void * /*user_data*/) -> bool {
+                            return true;
+                          });
         msgpack::object obj = oh.get();
         return obj.as<T>();
       } catch (msgpack::type_error& e) {
