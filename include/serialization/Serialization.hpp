0e90c679ff2bab9bf7f4c2e9c3659f2e497d3d84
Eric Flumerfelt <eflumerf@fnal.gov>
2025-02-10 09:16:48 -0600

    Include logging/Logging.hpp whenever ERS issues are declared, including note on why (TLOG() << ERSIssue only works when Logging.hpp included first)

--- include/serialization/Serialization.hpp
+++ include/serialization/Serialization.hpp
@@ -11,6 +11,7 @@
 #define SERIALIZATION_INCLUDE_SERIALIZATION_SERIALIZATION_HPP_
 
 #include "ers/Issue.hpp"
+#include <logging/Logging.hpp> // NOTE: if ISSUES ARE DECLARED BEFORE include logging/Logging.hpp, TLOG_DEBUG<<issue wont work.
 
 #include "boost/preprocessor.hpp"
 #include "msgpack.hpp"
